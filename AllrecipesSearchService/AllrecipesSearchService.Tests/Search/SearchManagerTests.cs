96a323fe464683bf3829471b042b72a4f452640f
Ed Castaneda <ed.castaneda@gmail.com>
2014-01-30 00:50:15 -0800

    GREEN LIGHT! (with mocks)

--- AllrecipesSearchService/AllrecipesSearchService.Tests/Search/SearchManagerTests.cs
+++ AllrecipesSearchService/AllrecipesSearchService.Tests/Search/SearchManagerTests.cs
@@ -1,5 +1,7 @@
-﻿using Allrecipes.CoreClasses.Search.Queries;
+﻿using Allrecipes.CoreClasses.Search;
+using Allrecipes.CoreClasses.Search.Queries;
 using AllrecipesSearchService.Search;
+using Moq;
 using NUnit.Framework;
 
 namespace AllrecipesSearchService.Tests.Search
@@ -12,14 +14,39 @@ namespace AllrecipesSearchService.Tests.Search
         {
             var query = BuildKeywordQuery("chicken");
 
-            var sut = new SearchManager();
+            var gsaQuery = "I'm a GSA QUERY!";
+            var queryBuilder = new Mock<IQueryBuilder>();
+            queryBuilder.Setup(qb => qb.CreateFromRecipeQuery(query)).Returns(gsaQuery);
+            var gsaResults = "I'm some gsa results in an xml formatted string!";
+            var queryEngine = new Mock<IQueryEngine>();
+            queryEngine.Setup(qe => qe.ExecuteQuery(gsaQuery)).Returns(gsaResults);
+            var resultsConverter = new Mock<IResultsConverter>();
+            resultsConverter.Setup(rc => rc.CreateFromProviderResults(gsaResults)).Returns(CreateFakeResults());
+
+            var sut = new SearchManager(queryBuilder.Object, queryEngine.Object, resultsConverter.Object);
 
             var results = sut.Search(query);
 
             Assert.NotNull(results);
+            Assert.That(results.MainFileCount > 1);
+            //Assert.That(results.MainIDs.Length > 1);
+            //Assert.That(results.MainItems.Length > 1);
+            //Assert.That(results.MainFileCount == results.MainIDs.ToList().Count);
+            //Assert.That(results.MainFileCount == results.MainItems.Count());
 
         }
 
+        private Results CreateFakeResults()
+        {
+            var results = new Results();
+            results.MainFileCount = 2;
+            //results.MainIDs[0] = 1;
+            //results.MainIDs[1] = 2;
+            //results.MainItems[0] = new ResultItem {ID = 1};
+            //results.MainItems[1] = new ResultItem { ID = 1 };
+
+            return results;
+        }
         private RecipeQuery BuildKeywordQuery(string searchText)
         {
             return new RecipeQuery { CollectionID = 12, SearchWith = searchText };
--- AllrecipesSearchService/AllrecipesSearchService/Search/SearchManager.cs
+++ AllrecipesSearchService/AllrecipesSearchService/Search/SearchManager.cs
@@ -6,18 +6,70 @@ namespace AllrecipesSearchService.Search
 {
     public class SearchManager
     {
+        private readonly IQueryBuilder _queryBuilder;
+        private readonly IQueryEngine _queryEngine;
+        private readonly IResultsConverter _resultsConverter;
+
+        public SearchManager(IQueryBuilder queryBuilder, IQueryEngine queryEngine, IResultsConverter resultsConverter)
+        {
+            _queryBuilder = queryBuilder;
+            _queryEngine = queryEngine;
+            _resultsConverter = resultsConverter;
+        }
 
         public Results Search(RecipeQuery recipeQuery)
         {
             Results results = null;
 
             // create query
+            var query = _queryBuilder.CreateFromRecipeQuery(recipeQuery);
 
             // query
+            var queryResults = _queryEngine.ExecuteQuery(query);
 
             // create results
+            results = _resultsConverter.CreateFromProviderResults(queryResults);
 
             return results;
+        }
+    }
+
+    public interface IResultsConverter
+    {
+        Results CreateFromProviderResults(string providerResults);
+    }
+
+    public class GSAResultsConverter : IResultsConverter
+    {
+        public Results CreateFromProviderResults(string providerResults)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IQueryEngine
+    {
+        string ExecuteQuery(string query);
+    }
+
+    public class GSAQueryEngine : IQueryEngine
+    {
+        public string ExecuteQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IQueryBuilder
+    {
+        string CreateFromRecipeQuery(RecipeQuery recipeQuery);
+    }
+
+    public class GSAQuery : IQueryBuilder
+    {
+        public string CreateFromRecipeQuery(RecipeQuery recipeQuery)
+        {
+            throw new NotImplementedException();
         }
     }
 }
